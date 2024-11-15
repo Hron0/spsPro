56151c0cc90e7fb7928ae9c794537d139d0513b8
Hron0 <sasha1254370@gmail.com>
2024-11-15 07:06:59 +0300

    UPD Disabled Auth temp

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,4 @@
+/*
 import NextAuth from "next-auth"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import { db } from "@/backend/db/index"
@@ -11,4 +12,4 @@ export const {
   session: { strategy: "jwt" },
   trustHost: true,
   ...authConfig,
-})
+})*/
