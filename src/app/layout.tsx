cceaaf21939ea021aada4326278e8891a3d60685
Hron0 <sasha1254370@gmail.com>
2024-11-15 04:33:45 +0300

    CHNG Layout Fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,58 +1,55 @@
-import { QueryProvider } from "@/components/providers/query-provider"
-import { ThemeProvider } from "@/components/providers/theme-provider"
-import { siteConfig } from "../../config/site"
-import { Toaster } from "@/components/ui/sonner"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import { type PropsWithChildren } from "react"
+import {QueryProvider} from "@/components/providers/query-provider"
+import {ThemeProvider} from "@/components/providers/theme-provider"
+import {siteConfig} from "../../config/site"
+import {Toaster} from "@/components/ui/sonner"
+import type {Metadata} from "next"
+import {Inter} from "next/font/google"
+import {type PropsWithChildren} from "react"
 import "@/globals.css"
+import {Navbar} from "@/components/shared/Navbar";
 
 const font = Inter({
-  weight: ["300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin-ext"],
+    weight: ["300", "400", "500", "600", "700", "800", "900"],
+    subsets: ["latin-ext"],
 })
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.title,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  openGraph: {
-    title: siteConfig.title,
+    title: {
+        default: siteConfig.title,
+        template: `%s - ${siteConfig.name}`,
+    },
     description: siteConfig.description,
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
+    openGraph: {
+        title: siteConfig.title,
+        description: siteConfig.description,
+    },
+    icons: {
+        icon: "/favicon.ico",
+        shortcut: "/favicon-16x16.png",
+        apple: "/apple-touch-icon.png",
+    },
 }
 
-export default function RootLayout({ children, modal }: {
-  children: React.ReactNode,
-  modal: React.ReactNode,
+export default function RootLayout({children, modal}: {
+    children: React.ReactNode,
+    modal: React.ReactNode,
 }) {
-  return (
-    <html lang="en" className={font.className} suppressHydrationWarning>
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <link rel="theme-color" href="#14B8A6" />
-      </head>
-      <body>
+    return (
+        <html lang="en" className={font.className} suppressHydrationWarning>
+        <head>
+            <title>dog</title>
+            <link rel="manifest" href="/manifest.json"/>
+            <link rel="apple-touch-icon" href="/icon.png"/>
+            <link rel="theme-color" href="#14B8A6"/>
+        </head>
+        <body className={"bg-background container flex flex-col items-center"}>
         <QueryProvider>
-          <ThemeProvider
-            attribute="class"
-            enableColorScheme
-            disableTransitionOnChange
-          >
+            <Navbar/>
             {children}
             {modal}
-            <Toaster />
-          </ThemeProvider>
+            <Toaster/>
         </QueryProvider>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
