65fe4428a1bec38ae635edd187f60aad8073f035
Hron0 <sasha1254370@gmail.com>
2024-11-16 22:24:09 +0300

    UPD responsible first Section

--- src/components/shared/mainPage/sections/First.tsx
+++ src/components/shared/mainPage/sections/First.tsx
@@ -6,16 +6,14 @@ import Image from "next/image";
 export const First = () => {
     return (
         <section
-            className="mx-auto w-full bg-secondary relative grid grid-rows-4 justify-center items-end justify-items-center gap-16 text-wite px-24 aspect-video">
-            <h1 className={"text-5xl font-bold text-white z-10 row-span-2"}>Честная судебная экспертиза</h1>
-            <h3 className={"text-2xl text-center text-white font-light z-10 whitespace-pre-line break-words self-center"}>
-                АНО «Межрегиональный центр независимых исследований, экспертиз <br/>
-                и права «Лучшее решение» <br/>
-                предлагает услуги по проведению внесудебных исследований, <br/>
-                судебных экспертиз и <br/>
-                рецензированию судебных экспертиз.</h3>
+            className="mx-auto w-full bg-secondary relative grid grid-rows-3 lg:grid-rows-4 justify-center items-end justify-items-center gap-16 px-4 text-white lg:px-24 aspect-video">
+            <h1 className={"text-3xl lg:text-5xl font-bold z-10 lg:row-span-2 text-center"}>Честная судебная экспертиза</h1>
+            <h3 className={"text-2xl text-center font-light z-10 whitespace-pre-line break-words self-center"}>
+                АНО «Межрегиональный центр независимых исследований, экспертиз и права «Лучшее решение» предлагает
+                услуги по проведению внесудебных исследований,
+                судебных экспертиз и рецензированию судебных экспертиз.</h3>
 
-            <Button className={"text-2xl z-10 font-light justify-self-start self-start relative mt-10 -left-[18%]"}
+            <Button className={"text-2xl z-10 font-light justify-self-start self-start relative mt-10"}
                     variant={"default"}
                     size={"lg"}
             >
