51403cbbf60b3f6e52d3d1f9e208d5dc76769851
Hron0 <sasha1254370@gmail.com>
2024-11-16 19:06:57 +0300

    SideBar types

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,17 @@ let config: {
                 accent: { foreground: string; DEFAULT: string };
                 destructive: { foreground: string; DEFAULT: string };
                 card: { foreground: string; DEFAULT: string };
-                primary: { foreground: string; DEFAULT: string }
+                primary: { foreground: string; DEFAULT: string };
+                sidebar: {
+                    DEFAULT: string
+                    foreground: string
+                    primary: string
+                    "primary-foreground": string
+                    accent: string
+                    "accent-foreground": string
+                    border: string
+                    ring: string
+                };
             };
             animation: { "accordion-up": string; "accordion-down": string }
         };
@@ -79,16 +89,16 @@ config = {
                     DEFAULT: "hsl(var(--card))",
                     foreground: "hsl(var(--card-foreground))",
                 },
-				sidebar: {
-					DEFAULT: 'hsl(var(--sidebar-background))',
-					foreground: 'hsl(var(--sidebar-foreground))',
-					primary: 'hsl(var(--sidebar-primary))',
-					'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-					accent: 'hsl(var(--sidebar-accent))',
-					'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-					border: 'hsl(var(--sidebar-border))',
-					ring: 'hsl(var(--sidebar-ring))'
-				},
+                sidebar: {
+                    DEFAULT: 'hsl(var(--sidebar-background))',
+                    foreground: 'hsl(var(--sidebar-foreground))',
+                    primary: 'hsl(var(--sidebar-primary))',
+                    'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+                    accent: 'hsl(var(--sidebar-accent))',
+                    'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+                    border: 'hsl(var(--sidebar-border))',
+                    ring: 'hsl(var(--sidebar-ring))'
+                },
             },
             borderRadius: {
                 sm: "var(--radius)",
