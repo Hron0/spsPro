9fed3892884754dd3251e831c46b5ee5a4a9d6c3
Hron0 <sasha1254370@gmail.com>
2024-11-17 23:45:55 +0300

    Fixed responsibility

--- src/components/shared/TitleHeader.tsx
+++ src/components/shared/TitleHeader.tsx
@@ -5,7 +5,7 @@ import {cn} from "@/lib/utils";
 function TitleHeader({title, color, className}: { title: string, color?: string, className?: string }) {
     return (
         <div
-            className={cn(color == 'orange' ? 'bg-black/20' : 'bg-black/40', "flex flex-row items-center justify-start z-10 relative h-16 lg:h-[100px] w-full lg:w-[480px]", className)}>
+            className={cn(color == 'orange' ? 'bg-black/20' : 'bg-black/40', "flex flex-row items-center justify-start z-10 relative h-16 lg:h-[100px] w-full md:w-[480px]", className)}>
             <div className={"relative h-full w-[40px] lg:w-[50px] z-20 right-4 lg:right-6"}>
                 <Image src={'/img/mainPage/column.png'}
                        alt={'Pic'}
