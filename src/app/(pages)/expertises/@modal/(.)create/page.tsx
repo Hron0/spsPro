76be51f844cd614b97f4393d950023b433b662b6
Hron0 <sasha1254370@gmail.com>
2024-12-11 01:42:12 +0300

    Expertises Routing

---
+++ src/app/(pages)/expertises/@modal/(.)create/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+import { NextPage } from 'next'
+import Modal from '@/components/shared/Modal'
+
+
+const Page: NextPage = () => {
+
+    return (
+        <Modal>
+            <div className="p-32 bg-white text-red-400 text-3xl">
+                RRRRRRRRRAAAAGHGG
+            </div>
+        </Modal>
+    )
+}
+
+export default Page
---
+++ src/app/(pages)/expertises/create/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { NextPage } from 'next'
+
+const Page: NextPage = () => {
+
+    return (
+       <div className={'text-red-400 text-xl h-full flex items-center justify-center'}>
+           RRRRRAAAGH
+       </div>
+    )
+}
+
+export default Page
