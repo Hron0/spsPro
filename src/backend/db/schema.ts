b07817ce620118d4a4b37c51e9f286918cef1538
Hron0 <sasha1254370@gmail.com>
2024-12-18 02:26:45 +0300

    Expertise table & Test API

--- src/backend/db/schema.ts
+++ src/backend/db/schema.ts
@@ -1,19 +1,19 @@
 import {
-    boolean,
+    boolean, date, jsonb,
     pgEnum,
     pgTable,
     serial,
     text,
     timestamp
 } from "drizzle-orm/pg-core"
-
+import { sql } from "drizzle-orm";
 export const roles = pgEnum('roles', ["USER", "ADMIN"])
 
 export const user = pgTable("user", {
     id: text("id")
         .primaryKey()
         .$defaultFn(() => crypto.randomUUID()),
-    login: text("login"),
+    login: text("login").unique(),
     password: text('password'),
     email: text("email").unique(),
     emailVerified: timestamp("emailVerified", {mode: "date"}),
@@ -27,4 +27,18 @@ export const TodoTable = pgTable("todos", {
     text: text("text").notNull(),
     done: boolean("done").default(false).notNull(),
     createdAt: timestamp("createdAt").defaultNow(),
+})
+
+export const Expertise = pgTable('expertise', {
+    id: serial('id').primaryKey(),
+    title: text('title'),
+    name: text('name'),
+    against: text('against'),
+    case: text('case'),
+    city: text('city'),
+    address: text('address'),
+    questions: jsonb('questions'),
+    types: text('types'),
+    date: date('date', {mode: "date"}).default(sql`now()`),
+    img: text('img'),
 })
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,59 +1,66 @@
-import { desc, eq } from "drizzle-orm"
-import { Hono } from "hono"
-import { db } from "./db"
-import { TodoTable } from "./db/schema"
+import {desc, eq} from "drizzle-orm"
+import {Hono} from "hono"
+import {db} from "./db"
+import {TodoTable} from "./db/schema"
 
 // init app
 export const app = new Hono().basePath("/api")
 
 // get all todos
-app.get("/todos", async ({ req, json }) => {
-  const { filter } = req.query()
-  switch (filter) {
-    case "all":
-      return json(
-        await db.select().from(TodoTable).orderBy(desc(TodoTable.createdAt))
-      )
-    case "done":
-      return json(
-        await db.select().from(TodoTable).where(eq(TodoTable.done, true))
-      )
-    case "undone":
-      return json(
-        await db.select().from(TodoTable).where(eq(TodoTable.done, false))
-      )
-  }
+app.get("/todos", async ({req, json}) => {
+    const {filter} = req.query()
+    switch (filter) {
+        case "all":
+            return json(
+                await db.select().from(TodoTable).orderBy(desc(TodoTable.createdAt))
+            )
+        case "done":
+            return json(
+                await db.select().from(TodoTable).where(eq(TodoTable.done, true))
+            )
+        case "undone":
+            return json(
+                await db.select().from(TodoTable).where(eq(TodoTable.done, false))
+            )
+    }
 })
+
+app.get("/expertise/:id", async ({req, json}) => {
+    const id = req.param('id')
+
+    return json(`Passed id is: ${id}`)
+})
+
 // create new todo
-app.post("/todos", async ({ req, json }) => {
-  const { text } = await req.json()
-  const newTodo = await db
-    .insert(TodoTable)
-    .values({ text })
-    .returning({ id: TodoTable.id, text: TodoTable.text, done: TodoTable.done })
-  return json(newTodo[0])
+app.post("/todos", async ({req, json}) => {
+    const {text} = await req.json()
+    const newTodo = await db
+        .insert(TodoTable)
+        .values({text})
+        .returning({id: TodoTable.id, text: TodoTable.text, done: TodoTable.done})
+    return json(newTodo[0])
 })
-// delete todo by id
-app.delete("/todos/:id", async ({ req, json }) => {
-  const id = await req.param("id")
-  const deleted = await db
-    .delete(TodoTable)
-    .where(eq(TodoTable.id, parseInt(id)))
-  return json({ msg: "todo has been deleted" })
+// delete gettodo by id
+app.delete("/todos/:id", async ({req, json}) => {
+    const id = await req.param("id")
+    const deleted = await db
+        .delete(TodoTable)
+        .where(eq(TodoTable.id, parseInt(id)))
+    return json({msg: "todo has been deleted"})
 })
 // clear all todos
-app.delete("/todos", async ({ json }) => {
-  const deleted = await db.delete(TodoTable)
-  return json({ msg: "todos has been cleared" })
+app.delete("/todos", async ({json}) => {
+    const deleted = await db.delete(TodoTable)
+    return json({msg: "todos has been cleared"})
 })
 // done todo
-app.patch("/todos/:id", async ({ req, json }) => {
-  const id = await req.param("id")
-  const { done } = await req.json()
-  const updated = await db
-    .update(TodoTable)
-    .set({ done })
-    .where(eq(TodoTable.id, parseInt(id)))
-    .returning({ id: TodoTable.id, text: TodoTable.text, done: TodoTable.done })
-  return json({ msg: "todo was updated" })
+app.patch("/todos/:id", async ({req, json}) => {
+    const id = await req.param("id")
+    const {done} = await req.json()
+    const updated = await db
+        .update(TodoTable)
+        .set({done})
+        .where(eq(TodoTable.id, parseInt(id)))
+        .returning({id: TodoTable.id, text: TodoTable.text, done: TodoTable.done})
+    return json({msg: "todo was updated"})
 })
