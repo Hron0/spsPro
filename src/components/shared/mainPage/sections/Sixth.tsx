b65a21a1f31eea811439f899f4daf719480f17d3
Hron0 <sasha1254370@gmail.com>
2024-12-18 02:28:31 +0300

    Refactored thing that gets all images in dir

--- src/components/shared/mainPage/sections/Sixth.tsx
+++ src/components/shared/mainPage/sections/Sixth.tsx
@@ -2,31 +2,43 @@
 import React from 'react';
 import Image from "next/image";
 import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel";
-import {getAllFilesSync} from "get-all-files";
 import TitleHeader from "@/components/shared/TitleHeader";
-
-// TODO: Я в ахуе
+import fs from "node:fs/promises";
 
 export default async function Sixth() {
-    const docs = getAllFilesSync(`public/Docs/mainPage/`).toArray();
-    const images = getAllFilesSync(`public/Docs/img/`).toArray();
-    for (let i = 0; i < docs.length; i++) {
-        docs[i] = docs[i].slice(7);
-    }
-    for (let i = 0; i < images.length; i++) {
-        images[i] = images[i].slice(6);
-        images[i] = images[i].replace('\\', '')
-    }
-    const docsImg = docs.map((item, index) => {
-        return {
-            doc: item,
-            img: images[index]
-        }
-    })
+    // const docs = getAllFilesSync(`public/Docs/mainPage/`).toArray();
+    // const images = getAllFilesSync(`public/Docs/img/`).toArray();
+    // for (let i = 0; i < docs.length; i++) {
+    //     docs[i] = docs[i].slice(7);
+    // }
+    // for (let i = 0; i < images.length; i++) {
+    //     images[i] = images[i].slice(6);
+    //     images[i] = images[i].replace('\\', '')
+    // }
+    // const docsImg = docs.map((item, index) => {
+    //     return {
+    //         doc: item,
+    //         img: images[index]
+    //     }
+    // })
+
+
+    const fileImages = await fs.readdir("public/Docs/img/");
+    const fileDocument = await fs.readdir("public/Docs/mainPage/");
+    const docsImg = fileDocument
+        .map((item, index) => {
+            return {
+                doc: `/Docs/mainPage/${item}`,
+                img: `/Docs/img/${fileImages[index]}`,
+                altImg: `${item}`,
+            }
+        });
+
 
 
     return (
-        <section className="mx-auto w-full container relative flex flex-col items-start gap-2 px-6 md:px-10 py-4 md:py-8">
+        <section
+            className="mx-auto w-full container relative flex flex-col items-start gap-2 px-6 md:px-10 py-4 md:py-8">
             <TitleHeader title={'Официальные документы'} color={'orange'}/>
             <div className={"relative w-full px-12"}>
 
@@ -42,7 +54,7 @@ export default async function Sixth() {
                                 <div className="p-1">
                                     <a href={doc.doc} target="_blank">
                                         <Image src={doc.img}
-                                               alt={'Dog'}
+                                               alt={doc.altImg}
                                                width={262}
                                                height={354}/>
                                     </a>
