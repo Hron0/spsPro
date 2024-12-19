4cdd01a775776f1451614c7386b0ec081b600438
Hron0 <sasha1254370@gmail.com>
2024-12-19 19:55:40 +0300

    add ExpertiseSchema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -21,6 +21,34 @@ export const RegisterSchema = z.object({
     })
 })
 
+export const ExpertiseSchema = z.object({
+    title: z.string().min(1, {
+        message: "Введите название Экспертизы"
+    }),
+    name: z.string().min(1, {
+        message: "Введите Имя/ФИО агента"
+    }),
+    against: z.string().min(1, {
+        message: "Введите Контрагента"
+    }),
+    case: z.string().min(1, {
+        message: "Введите номер дела"
+    }),
+    city: z.string().min(1, {
+        message: "Введите Город"
+    }),
+    address: z.string().min(1, {
+        message: "Введите Адрес"
+    }),
+    // questions: z.array(z.string()),
+    questions: z.string().min(1, {
+        message: "Введите вопросы по экспертизе"
+    }),
+    types: z.string().min(1, {
+        message: "Введите вид(ы) экспертизы."
+    }),
+})
+
 const MAX_FILE_SIZE = 1024 * 1024 * 5;
 const ACCEPTED_IMAGE_MIME_TYPES = [
     "image/jpeg",
