fea73ad168c1813cb7a3a33b83deebc637cd1a12
Hron0 <sasha1254370@gmail.com>
2025-01-23 13:39:52 +0300

    SearchBar w/Icon comp

---
+++ src/app/(pages)/blog/create/SearchBar.tsx
@@ -0,0 +1,16 @@
+import {Input} from "@/components/ui/input";
+import { Search } from 'lucide-react'
+
+interface SearchBarProps {
+    onSearch: (query: string) => void
+}
+
+export function SearchBar({ onSearch }: SearchBarProps) {
+    return (
+        <div className="relative w-1/3 my-4">
+            <Search className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-500"/>
+            <Input type="text" placeholder="Поиск..." onChange={(e) => onSearch(e.target.value)}
+                   className="pl-8"/>
+        </div>
+    );
+}
