08e12473a5a2be7869a2379de12fcc7e54f41389
hrono <sasha1254370@gmail.com>
2025-03-03 04:17:22 +0000

    DogFix pt-1

--- src/app/(pages)/expertises/(expDeatils)/[id]/page.tsx
+++ src/app/(pages)/expertises/(expDeatils)/[id]/page.tsx
@@ -6,13 +6,13 @@ import {notFound} from "next/navigation";
 import {ExpertiseType} from "@/lib/types/expertises";
 
 
-export async function generateStaticParams() {
-    const expertises = await fetch(process.env["NEXT_PUBLIC_URL"] + '/api/expertises/all', {method: "GET"}).then((res) => res.json())
-
-    return expertises.map((item: any) => ({
-        id: item.id.toString()
-    }))
-}
+// export async function generateStaticParams() {
+//     const expertises = await fetch(process.env["NEXT_PUBLIC_URL"] + '/api/expertises/all', {method: "GET"}).then((res) => res.json())
+//
+//     return expertises.map((item: any) => ({
+//         id: item.id.toString()
+//     }))
+// }
 
 async function Page({params}: { params: { id: any } }) {
     const id = params.id
